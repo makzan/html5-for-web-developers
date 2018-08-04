229102682bef12d908a7d2a8aa821d518e131cb0
Thomas Seng Hin Mak <mak@makzan.net>
2018-08-04 21:20:52 +0800

    Init the next.js project.

---
+++ pages/about.jsx
@@ -0,0 +1,9 @@
+import Header from '../components/Header'
+
+export default () => (
+  <div>
+    <Header />
+    <h1>About</h1>
+    <p>This is a course book created by <a href="https://makzan.net">Makzan</a></p>
+  </div>
+)
---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+import Header from '../components/Header'
+
+export default () => (
+  <div>
+    <Header />
+    <p>HTML5 for Web Developers <small>(CM419-08-2018)</small></p>
+  </div>
+)
